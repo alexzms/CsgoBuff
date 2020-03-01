cead97372c75a0bc891e68ad80ee1f133888f56c
alexzms <zhangminshen@yeah.net>
2020-03-01 16:29:39 +0800

    csgolap ver2.0 des: added sorting order cutomize and page range customize

--- CSGOLAP.py
+++ CSGOLAP.py
@@ -11,13 +11,17 @@ import plotly_express as px
 import plotly
 import sys
 
+#todo list: 
+# 0:sticker analyze
+# 8:better hover
+# 9:login system
 
 def login(username,password):
     #todo
     pass
 
-def getItem(goods_id,page_num,headers,mode):
-    payload = {'game':'csgo','goods_id':goods_id,'page_num':str(page_num),'sort_by':'default','mode':'','allow_tradable_cooldown':'1','_':''}
+def getItem(goods_id,page_num,headers,mode,sort):
+    payload = {'game':'csgo','goods_id':goods_id,'page_num':str(page_num),'sort_by':sort,'mode':'','allow_tradable_cooldown':'1','_':''}
     r = requests.get('https://buff.163.com/api/market/goods/sell_order',params = payload,headers=headers)
     js = r.json()
     if mode == 1:
@@ -54,28 +58,54 @@ def flatten(a):
 
 def main():
     print('Counter-Strike Lowest Actual Price Counter')
-    if len(sys.argv) != 3:
+    sort_method = ''
+    if len(sys.argv) != 4:
         analyze = int(input("请输入你要分析的csgo饰品id: "))
-        page_full = int(input("请输入你要分析的csgo饰品数量(x10): "))
+        page_full = input("请输入你要分析的csgo饰品数量(x10)(格式：{起始页-终止页}): ")
+        sort_method = input("请输入排序顺序(price(低到高),priced(高到低),wear,weard,new,old): ")
     else:
         analyze = int(sys.argv[1])
-        page_full = int(sys.argv[2])
+        page_full = sys.argv[2]
+        sort_method = sys.argv[3]
+        print ('已从启动参数获得参数')
+
+    page_split = page_full.split('-')
+    if len(page_split) != 2:
+        print ("error - page input invalid")
+        os.system('pause')
+        return
+
+    if sort_method == 'price':
+        sort_method = 'price.asc'
+    elif sort_method == 'priced':
+        sort_method = 'price.desc'
+    elif sort_method == 'wear':
+        sort_method = 'wear.asc'
+    elif sort_method == 'wear':
+        sort_method = 'wear.desc'
+    elif sort_method == 'new':
+        sort_method = 'created.desc'
+    elif sort_method == 'old':
+        sort_method = 'created.asc'
+
+    page_start = int(page_split[0])
+    page_end = int(page_split[1])
     login('','')
     data_all = []
     headers={
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
         'Referer':'https://buff.163.com/',
-        'Cookie':'mail_psc_fingerprint=01fde5382cc83a2df5599647c6b1bccc; _ntes_nnid=8f32ece229cabf2b415c5d84bed54e45,1553513994953; _ntes_nuid=8f32ece229cabf2b415c5d84bed54e45; usertrack=CrHum1ynbCoMKKIsAwU+Ag==; vinfo_n_f_l_n3=549968d4ed4cfbe3.1.0.1570117734269.0.1570117797098; nts_mail_user=pumusy4546997@163.com:-1:1; Device-Id=4zwPlnjIQUO6zLQtEYTy; _ga=GA1.2.1629007142.1575890972; _ns=NS1.2.255152277.1581691039; Locale-Supported=zh-Hans; game=csgo; _gid=GA1.2.1894175785.1582985758; NTES_YD_SESS=Ga.vA045gNGC9CgP_xTvS9MBsXCHriUmW.VlWS06WQXd5EnP5.MfeYbDXZ7skRTn.mzOibeTobgqjU.V0x6YfmWDCaM1D9Fz.wFLXlz4oGEilolLgN2BWWRP93swQLuq73dGG800mI6MYGl2.akzO1lrgeRUinXlCMXfS_gAk_ZHSCwmC_8fAoHiZvZzhlGHqq02jUfJ6j5GgxqXMw.XjvUTlKUL59kD4wS7lRJuBR5wO; S_INFO=1582985877|0|3&80##|19946167252; P_INFO=19946167252|1582985877|0|netease_buff|00&99|shh&1579923113&netease_buff#shh&null#10#0#0|&0|null|19946167252; session=1-Sqz4Loze6hIo69NmH8Oxe5t6iQKEvPysQD_ZTGn3tyjG2046555823; _gat_gtag_UA_109989484_1=1; csrf_token=ImRjZWRlMTI2N2QwYzRlNTU2OTgyY2I4ZDdiNzdiMjk2MTBjMmRjYzEi.ETwHCA.n9lwH7yd6lDR_2pbJICdMUDCANg'
+        'Cookie':'mail_psc_fingerprint=01fde5382cc83a2df5599647c6b1bccc; _ntes_nnid=8f32ece229cabf2b415c5d84bed54e45,1553513994953; _ntes_nuid=8f32ece229cabf2b415c5d84bed54e45; usertrack=CrHum1ynbCoMKKIsAwU+Ag==; vinfo_n_f_l_n3=549968d4ed4cfbe3.1.0.1570117734269.0.1570117797098; nts_mail_user=pumusy4546997@163.com:-1:1; Device-Id=4zwPlnjIQUO6zLQtEYTy; _ga=GA1.2.1629007142.1575890972; _ns=NS1.2.255152277.1581691039; _gid=GA1.2.1894175785.1582985758; Locale-Supported=zh-Hans; game=csgo; NTES_YD_SESS=dZkpmkRbjfofGqz7K6d0JlwEVZ5PB2MaHg0Hk14NkUVuqC5iqgFBt6mlVSfaGJ35gjOPhmt3ImLZv9g04EN6Bjkl2oF_lXpOgxpDVHObIdChHIHDLRnskkJiXWaxUDQZfWuddK44jwNF6dHngoGOP_HTLtJ9h5VH2FVB1ALzGASVo_0aR0lZ83puX7epzqxuiycmvpxRPryicYElig8c_T93Hc9DqXGlbx1fHJ.QsJqxP; S_INFO=1583039533|0|3&80##|19946167252; P_INFO=19946167252|1583039533|0|netease_buff|00&99|shh&1582985877&netease_buff#shh&null#10#0#0|&0|null|19946167252; session=1-xA6RcIV194s9igG5h6C4JLinp6VbUMm--sOj26fBHCRz2046555823; csrf_token=IjQ3ODE3Njc3MTJmOWI3ZTk0OTcxMjU1YzQ0NmM0MWM0ODliOTFhYzMi.ETzV4g.VJNpFoxFut2Pya1wwCBVP5jlbT4'
     }
-    itemName = getItem(analyze,1,headers,2)[0]
+    itemName = getItem(analyze,1,headers,2,'default')[0]
     # print(price)
     # print(wear)
     # price_draw = np.array(flatten(price))
     # wear_draw = np.array(flatten(wear))
     # plt.scatter(wear_draw,price_draw,zorder = 0,picker = True)
     # plt.show()
-    for x in range(1,page_full+1):
-        data = getItem(analyze,x,headers,1)
+    for x in range(page_start,page_end+1):
+        data = getItem(analyze,x,headers,1,sort_method)
         data_all.append([itemName,data[1][0],data[0][0],x,1])
         data_all.append([itemName,data[1][1],data[0][1],x,2])
         data_all.append([itemName,data[1][2],data[0][2],x,3])
